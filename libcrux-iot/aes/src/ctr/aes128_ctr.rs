68fcc808fba7632a52c2e7005af49d1090f59d3d
robinhundt <24554122+robinhundt@users.noreply.github.com>
2026-09-08 18:48:08 +0200

    aes: fix misleading CTR_LEN asserts
    having a CTR_LEN of 8 would underflow &ctr.to_be_bytes()[4 - CTR_LEN..]
    in ctr.rs

--- libcrux-iot/aes/src/ctr/aes128_ctr.rs
+++ libcrux-iot/aes/src/ctr/aes128_ctr.rs
@@ -20,7 +20,7 @@ impl<T: AesCipherState, const CTR_LEN: usize, const NONCE_START: usize>
     pub(crate) fn init(key: &[u8], nonce: &[u8]) -> Self {
         assert_eq!(nonce.len(), NONCE_LEN);
         assert_eq!(key.len(), GCM_KEY_LEN);
-        assert!(CTR_LEN <= 8 && CTR_LEN > 1);
+        assert!(CTR_LEN <= 4 && CTR_LEN > 1);
 
         let mut ctr_nonce = [0u8; 16];
         if NONCE_START == 1 {
